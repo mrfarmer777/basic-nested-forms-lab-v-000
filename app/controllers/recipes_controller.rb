3a3dbb9f6e760293a00a2bbd5762ace78cc36443
Matt Farmer <mrfarmer777@gmail.com>
2018-07-16 04:15:49 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,8 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
-    @ing1 = Ingredient.new
-    @ing2 = Ingredient.new
+    @ing1 = @recipe.ingredients.build
+    @ing2 = @recipe.ingredients.build
   end
 
   def create
